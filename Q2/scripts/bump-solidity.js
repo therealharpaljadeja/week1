1b5722c342731c6e2ac1778c3bb1bb29adcaf698
therealharpaljadeja <jadejaharpal14@gmail.com>
2022-05-06 13:04:27 +0530

    bump-solidity.js modified

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -1,12 +1,55 @@
 const fs = require("fs");
-const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
+const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/;
 
-const verifierRegex = /contract Verifier/
+const verifierRegex = /contract Verifier/;
 
-let content = fs.readFileSync("./contracts/HelloWorldVerifier.sol", { encoding: 'utf-8' });
-let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
-bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
+let HelloWorldContent = fs.readFileSync("./contracts/HelloWorldVerifier.sol", {
+	encoding: "utf-8",
+});
+let HelloWorldBumped = HelloWorldContent.replace(
+	solidityRegex,
+	"pragma solidity ^0.8.0"
+);
+HelloWorldBumped = HelloWorldBumped.replace(
+	verifierRegex,
+	"contract HelloWorldVerifier"
+);
 
-fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
+fs.writeFileSync("./contracts/HelloWorldVerifier.sol", HelloWorldBumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+let Multiplier3Groth16 = fs.readFileSync(
+	"./contracts/Multiplier3Groth16Verifier.sol",
+	{ encoding: "utf-8" }
+);
+let Multiplier3Groth16Bumped = Multiplier3Groth16.replace(
+	solidityRegex,
+	"pragma solidity ^0.8.0"
+);
+Multiplier3Groth16Bumped = Multiplier3Groth16Bumped.replace(
+	verifierRegex,
+	"contract Multiplier3Groth16Verifier"
+);
+
+fs.writeFileSync(
+	"./contracts/Multiplier3Groth16Verifier.sol",
+	Multiplier3Groth16Bumped
+);
+
+let Multiplier3Plonk = fs.readFileSync(
+	"./contracts/Multiplier3PlonkVerifier.sol",
+	{ encoding: "utf-8" }
+);
+let Multiplier3PlonkBumped = Multiplier3Plonk.replace(
+	solidityRegex,
+	"pragma solidity ^0.8.0"
+);
+Multiplier3PlonkBumped = Multiplier3PlonkBumped.replace(
+	verifierRegex,
+	"contract Multiplier3PlonkVerifier"
+);
+
+fs.writeFileSync(
+	"./contracts/Multiplier3PlonkVerifier.sol",
+	Multiplier3PlonkBumped
+);
